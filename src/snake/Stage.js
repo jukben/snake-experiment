eaa93c4287c75e2a8bcd1e8521a7f5178aaa0716
Martin Jakubec <trubi@email.cz>
2018-03-24 15:57:04 +0100

    xx

--- src/snake/Stage.js
+++ src/snake/Stage.js
@@ -18,22 +18,14 @@ class Stage extends React.Component<Props> {
 
     render() {
         return (
-            <View style={{
-                flex: 1
-            }}>
+            <View style={styles.container}>
                 <View style={styles.stage}>
                     {this.props.snakes.map(snake => (
                         <SnakeComponent key={snake.id} snake={snake} />
                     ))}
                 </View>
 
-                <View style={{
-                    position: "absolute",
-                    bottom: 0,
-                    width: "100%",
-                    flexDirection: "row",
-                    justifyContent: "space-between",
-                }}>
+                <View style={styles.buttons}>
                     <View>
                         <Button onPress={() => this.props.clickLeft("snake-1")} title="LEFT" />
                         <Button onPress={() => this.props.clickRight("snake-1")} title="RIGHT" />
@@ -49,12 +41,27 @@ class Stage extends React.Component<Props> {
 }
 
 const styles = {
+    container: {
+        flex: 1,
+    },
     stage: {
+        position: "absolute",
+        bottom: 100,
+        top: 100,
+        left: 20,
+        right: 20,
         borderColor: "grey",
         borderWidth: 0.5,
         borderRadius: 10,
-        backgroundColor: "#eeeeee",
-    }
+        backgroundColor: "lightgrey",
+    },
+    buttons: {
+        position: "absolute",
+        bottom: 0,
+        width: "100%",
+        flexDirection: "row",
+        justifyContent: "space-between",
+    },
 }
 
 const mapStateToProps = (state: StateObject) => ({
